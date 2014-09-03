6af10c51219780652b211b235d60b676e472b48d
Tim <tim@maplejar.co.uk>
2014-09-03 06:55:51 +0100

    user changes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,4 +11,27 @@ class UserTest < ActiveSupport::TestCase
 
     assert User.create!( params );
   end
+
+  test "can't create user with same email address" do
+    params1 = {
+      name: "Mr Tester",
+      email: "foo@bar.com",
+      password: "changeme",
+      salt: "123456"
+    }
+
+    params2 = {
+      name: "Ms Peters",
+      email: "foo@bar.com",
+      password: "p4ssw0rd",
+      salt: "abcdef"
+    }
+
+    assert User.create! params1
+    assert_raises( ActiveRecord::RecordInvalid ) do
+      User.create! params2
+    end
+
+  end
+    
 end
