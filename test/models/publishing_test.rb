f5582ee83bf90a2444a4431a60f6c676189e4539
Tim <tim@maplejar.co.uk>
2014-09-21 09:28:51 +0100

    publishing test

--- test/models/publishing_test.rb
+++ test/models/publishing_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PublishingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create publishing" do
+    # Given
+    book = given_a_book_exists
+    author = given_an_author_exists
+    
+    # When # Then
+    assert_difference( "Publishing.count", 1 ) do
+      publishing = Publishing.create!( :book => book, :author => author )
+    end
+  end
+
+  should belong_to :book
+  should belong_to :author
 end
