9612be988755571968e5b2fd6438ee3155db9bca
Tim <tim@maplejar.co.uk>
2014-09-21 10:04:16 +0100

    publishing validates presence of publication_date

--- app/models/publishing.rb
+++ app/models/publishing.rb
@@ -1,4 +1,6 @@
 class Publishing < ActiveRecord::Base
   belongs_to :book
   belongs_to :author
+
+  validates_presence_of :publication_date
 end
--- test/models/publishing_test.rb
+++ test/models/publishing_test.rb
@@ -5,13 +5,15 @@ class PublishingTest < ActiveSupport::TestCase
     # Given
     book = given_a_book_exists
     author = given_an_author_exists
+    publication_date = given_a_random_date
     
     # When # Then
     assert_difference( "Publishing.count", 1 ) do
-      publishing = Publishing.create!( :book => book, :author => author )
+      publishing = Publishing.create!( :book => book, :author => author, :publication_date => publication_date )
     end
   end
 
   should belong_to :book
   should belong_to :author
+  should validate_presence_of :publication_date
 end
